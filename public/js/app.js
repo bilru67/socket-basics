f2e9ab36ffc990b2979de58956a998a17d92e88d
Bill Russo <bilru@live.com>
2017-08-01 06:51:10 -0400

    add message to browser ui

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ console.log('connected to socket.io server');
 socket.on('message', function(message){
 	console.log('New message:');
 	console.log(message.text);
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 });
 
 //handles submitting of new message
